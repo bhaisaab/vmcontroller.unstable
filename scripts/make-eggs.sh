24f9dcfb28cfa804d7e712d33b2b8c836eeeda4f
Rohit Yadav <rohityadav89@gmail.com>
2010-10-21 20:45:11 +0530

    Eggs making: minor changes

--- scripts/make-eggs.sh
+++ scripts/make-eggs.sh
@@ -5,5 +5,3 @@ buildout setup ../src/vmcontroller.common/setup.py bdist_egg -d $PWD/../dist
 buildout setup ../src/vmcontroller.host/setup.py bdist_egg -d $PWD/../dist
 
 buildout setup ../src/vmcontroller.vm/setup.py bdist_egg -d $PWD/../dist
-
-sh clean.sh
