32e8463e3845373cf0ca8acf4b610d2466feb258
Rohit Yadav <rohityadav89@gmail.com>
2010-10-27 00:29:22 +0530

    Renaming script for cooking boiled eggs

--- scripts/cook-eggs.sh
+++ scripts/cook-eggs.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 buildout setup ../src/vmcontroller.common/setup.py bdist_egg -d $PWD/../dist
-
 buildout setup ../src/vmcontroller.host/setup.py bdist_egg -d $PWD/../dist
-
 buildout setup ../src/vmcontroller.vm/setup.py bdist_egg -d $PWD/../dist
+echo "Eggs created in dist folder"
