58d1393529f75943f30709b1b493557d20c570b1
Rohit Yadav <rohityadav89@gmail.com>
2010-10-29 01:48:31 +0530

    New config files for vmc host

---
+++ src/vmcontroller.host/vmcontroller/host/config/__init__.py
@@ -0,0 +1,30 @@
+"""
+Configuration support functionality.
+
+"""
+
+import os
+import inject
+import logging
+
+from ConfigParser import SafeConfigParser
+from pkg_resources import resource_stream
+
+@inject.param('config')
+def init_config_file(config_file, config):
+    if config_file and os.path.exists(config_file):
+        read = config.read([config_file])
+        if not read:
+            raise ValueError("Could not read configuration from file: %s" % config_file)
+
+def init_config():
+    config = SafeConfigParser()
+    config.readfp(resource_stream(__name__, 'default.cfg'))
+    return config
+
+@inject.param('config')
+@inject.param('logger')
+def debug_config(config, logger):
+    for section in config.sections():
+        for option in config.options(section):
+            logger.debug("%s::%s=%s" % (section, option, config.get(section, option)))
