04b32b6d6f658981707dd4d803a328141ce41bf2
Rohit Yadav <rohityadav89@gmail.com>
2010-10-29 01:03:59 +0530

    Minor edits

--- scripts/cook-eggs.sh
+++ scripts/cook-eggs.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-buildout setup ../src/vmcontroller.common/setup.py bdist_egg -d $PWD/../dist
-buildout setup ../src/vmcontroller.host/setup.py bdist_egg -d $PWD/../dist
-buildout setup ../src/vmcontroller.vm/setup.py bdist_egg -d $PWD/../dist
-echo "Eggs created in dist folder"
+echo "Make sure you have buildout script in ../buildout/bin/"
+buildout=$PWD/../buildout/bin/buildout
+echo "using buildout: $buildout"
+$buildout setup ../src/vmcontroller.common/setup.py bdist_egg -d $PWD/../dist
+$buildout setup ../src/vmcontroller.host/setup.py bdist_egg -d $PWD/../dist
+$buildout setup ../src/vmcontroller.vm/setup.py bdist_egg -d $PWD/../dist
+echo "Eggs created in dist folder: $PWD/../dist"
