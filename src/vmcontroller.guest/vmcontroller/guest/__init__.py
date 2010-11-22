09a83fce2d71a95b53442c32c8043f4bc6bc35da
Rohit Yadav <rohityadav89@gmail.com>
2010-11-22 05:57:04 +0530

    Guest: main(), broker stuff etc. whole new incarnation

--- src/vmcontroller.guest/vmcontroller/guest/__init__.py
+++ src/vmcontroller.guest/vmcontroller/guest/__init__.py
@@ -2,14 +2,12 @@
 Guest Package
 """
 
-__version__ = '0.0.1'
-__authors__ = ['"Rohit Yadav" <rohityadav89@gmail.com>', 
-               '"David Garcia Quintas" <dgquintas@gmail.com>']
+__version__ = '0.1.0'
+__authors__ = ['"David Garcia Quintas" <dgquintas@gmail.com>', 
+               '"Rohit Yadav" <rohityadav89@gmail.com>']
 __copyright__ = "Copyright 2010 VMController Authors"
 __license__ = """Licensed under the (Simplified) BSD License
-you may not use this project "VMController" except in compliance with the License.
-You may obtain a copy of the License at
- 
+you may not use this project "VMController" except in compliance with the License. You may obtain a copy of the License at
   http://www.opensource.org/licenses/bsd-license.php
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
@@ -24,7 +22,13 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
+
 try:
-    from vmcontroller.guest.services import VMStompEngine
-except ImportError:
+    from __main__ import *
+except ImportError, e:
     pass
+
+#try:
+#    from vmcontroller.guest.services import VMStompEngine
+#except ImportError:
+#    pass
--- src/vmcontroller.guest/vmcontroller/guest/__main__.py
+++ src/vmcontroller.guest/vmcontroller/guest/__main__.py
@@ -1,19 +1,107 @@
 #!python
 """
-Entrypoint for starting the VMController VM module in guest OS.
+Entrypoint for starting the VMController Guest.
 """
 
-import os
-import sys
-import logging
+try:
+    import os
+    import sys
+    import logging
+    import tempfile
+    import inject
 
-logging.basicConfig(level=logging.DEBUG, \
-    format='%(asctime)s - %(name)s - %(levelname)s: %(message)s', )
+    from pkg_resources import resource_stream
+    from ConfigParser import SafeConfigParser
+    from optparse import OptionParser
+    from twisted.internet import reactor, protocol
+    from twisted.application import internet, service
 
-logger = lambda: logging.getLogger(__name__)
+    from vmcontroller.common import support, exceptions
+    from vmcontroller.common import StompProtocolFactory, StompProtocol
+    from vmcontroller.guest.config import init_config, init_config_file, debug_config
+    from vmcontroller.guest.services import VMStompEngine, VMWords
+except ImportError, e:
+    print "Import error in %s : %s" % (__name__, e)
+    import sys
+    sys.exit()
+
+logger = logging.getLogger(__name__)
+
+def init_logging(logfile=None, loglevel=logging.INFO):
+    """
+    Sets logging configuration.
+    @param logfile: File to log messages. Default is None.
+    @param loglevel: Log level. Default is logging.INFO.
+    """
+    format = '%(asctime)s - [%(threadName)s] %(filename)s:%(lineno)s - (%(levelname)s) %(message)s'
+    if logfile:
+        logging.basicConfig(filename=logfile, level=loglevel, format=format)
+    else:
+        logging.basicConfig(level=loglevel, format=format)
+
+def init():
+    """
+    """
+
+    parser = OptionParser()
+    parser.add_option("-c", "--config", dest="configfile",
+                      help="Read configuration from FILE. (Overrides default config file.)", metavar="FILE")
+    parser.add_option("-a", "--host", dest="broker_host",
+                      help="Listen on specified address for broker.", metavar="ADDR")
+    parser.add_option("-p", "--port", dest="broker_port",
+                      help="Listen on specified port for broker.", type="int", metavar="PORT")
+    parser.add_option("-l", "--logfile", dest="logfile",
+                      help="Log to specified file.", metavar="FILE")
+    parser.add_option("--debug", action="store_true", dest="debug", default=False, 
+                      help="Sets logging to debug (unless logging configured in config file).")
+
+    (options, args) = parser.parse_args()
+
+    config = init_config()
+    injector = inject.Injector()
+    inject.register(injector)
+
+    injector.bind('config', to=config)
+    injector.bind('words', to=VMWords.getWords, scope=inject.appscope)
+    injector.bind('stompProtocol', to=StompProtocol, scope=inject.appscope) 
+    injector.bind('stompEngine', to=VMStompEngine, scope=inject.appscope) 
+    injector.bind('subject', to=VMStompEngine, scope=inject.appscope) 
+
+    if options.broker_host is not None:
+        config.set('broker', 'host', options.broker_host)
+        
+    if options.broker_port is not None:
+        config.set('broker', 'port', str(options.broker_port))
+
+    level = logging.DEBUG if options.debug else logging.INFO
+    init_logging(logfile=options.logfile, loglevel=level)
+    debug_config(config)
+
+@inject.param('config')
+def start(config):
+    """Start twisted event loop and the fun should begin..."""
+
+    host = config.get('broker', 'host') 
+    port = int(config.get('broker', 'port'))
+    username = config.get('broker', 'username')
+    password = config.get('broker', 'password')
+
+    stompProtocolFactory = StompProtocolFactory()
+
+    #spawnChirpServerProcess(config)
+
+    reactor.connectTCP(host, port, stompProtocolFactory)
+    reactor.run()
+    return internet.TCPClient(host, port, stompProtocolFactory)
 
 def main():
-    logger().info("Starting VMController in guest.")
+    init()
+    logger.info("Starting VMController in guest.")
+
+    application = service.Application("vmcontroller.guest")
+
+    myservice = start()
+    myservice.setServiceParent(application)
 
 if __name__ == '__main__':
     try:
@@ -30,39 +118,6 @@ if __name__ == '__main__':
 
 
 
-from boincvm.common import support, Exceptions
-from boincvm.common import StompProtocolFactory, StompProtocol
-
-from boincvm.vm import VMStompEngine, VMWords
-
-from twisted.internet import reactor, protocol
-from twisted.application import internet, service
-
-from ConfigParser import SafeConfigParser
-import os
-import tempfile
-import logging
-import inject
-
-logging.basicConfig(level=logging.DEBUG, \
-    format='%(asctime)s - %(name)s - %(levelname)s: %(message)s', 
-    )
-
-def start(config):
-  """Start twisted event loop and the fun should begin..."""
-  host = config.get('Broker', 'host') 
-  port = int(config.get('Broker', 'port'))
-  username = config.get('Broker', 'username')
-  password = config.get('Broker', 'password')
-
-  
-  stompProtocolFactory = StompProtocolFactory()
-  
-  #spawnChirpServerProcess(config)
-
-  #reactor.connectTCP(host, port, stompProtocolFactory)
-  #reactor.run()
-  return internet.TCPClient(host, port, stompProtocolFactory)
 
 
 
@@ -135,28 +190,4 @@ def spawnChirpServerProcess(config):
 #
 #    config = SafeConfigParser()
 #    config.read(configFile)
-
-application = service.Application("boinc-vm-controller")
-
-
-
-configFile = '/etc/boinc-vm-controller/VMConfig.cfg' #FIXME: HARDCODED
-if not os.path.isfile(configFile):
-  configFile = os.path.basename(configFile) 
-
-config = SafeConfigParser()
-config.read(configFile)
-
-injector = inject.Injector()
-inject.register(injector)
-
-injector.bind('config', to=config )
-injector.bind('words', to=VMWords.getWords, scope=inject.appscope)
-injector.bind('stompProtocol', to=StompProtocol, scope=inject.appscope) 
-injector.bind('stompEngine', to=VMStompEngine, scope=inject.appscope) 
-injector.bind('subject', to=VMStompEngine, scope=inject.appscope) 
-
-
-service = start(config)
-service.setServiceParent(application)
 
